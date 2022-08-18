616e98c8b2dda8e869904d6b63e7d298de0463f8
Hong You Wong <hwong@u81z04s01.42kl.edu.my>
2022-08-18 18:34:21 +0800

    Commit 4 : reuploaded ex03 ~ ex04 with changes conforming to norminette

--- ex03/ft_print_numbers.c
+++ ex03/ft_print_numbers.c
@@ -1,12 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwong <hwong@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/18 15:34:54 by hwong             #+#    #+#             */
+/*   Updated: 2022/08/18 15:34:54 by hwong            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_print_numbers(void);
-
-void ft_print_numbers(void)
+void	ft_print_numbers(void)
 {
-    int num = 0;
-    while (num<=9){
-        write(1, &num, 1);
-        num++;
-    }
+	int	num;
+
+	num = 0;
+	while (num <= 9)
+	{
+		write(1, &num, 1);
+		num++;
+	}
+}
+
+int main(void)
+{
+	ft_print_numbers();
 }
